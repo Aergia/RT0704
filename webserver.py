2ff5acaafccd87a67dddec706460bd671e53c4a2
Lucas Desinde <lucas.desinde@etudiant.univ-reims.fr>
2024-01-28 19:58:47 +0000

    Routes

--- webserver.py
+++ webserver.py
@@ -1,12 +1,19 @@
 import json
+from flask import Flask, request
 import os
+import requests
 
-
+path = os.path.dirname(__file__)
+app = Flask(__name__)
 
 def createUserFile(username):
     file = openUserfile(username)
-
     json.dump([],file)
+    closeUserFile(file)
+
+def deleteUserFile(username):
+    if(os.path.exists(os.path.dirname(__file__)+"/user-data/"+username+".json")):
+        os.remove(os.path.dirname(__file__)+"/user-data/"+username+".json")
 
 def openUserfile(username):
     path = os.path.dirname(__file__)+"/user-data/"+username+".json"
@@ -16,4 +23,16 @@ def openUserfile(username):
 def closeUserFile(file):
     file.close()
 
-createUserFile("lapin")
+@app.route('/add', methods= ['POST'])
+def add():
+    name = str(request.get_data())[7:]
+    name = str(name[:int(len(name)-1)])
+    dicto = {}
+    dicto+=name
+    req = requests.post('http://localhost:8000/add', json=dicto)
+
+    
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8001)
